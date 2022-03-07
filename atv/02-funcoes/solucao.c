7027e19748210f888d9a6e6cd907026dde1522f4
Lihsayuri <liviasm1@al.insper.edu.br>
2022-03-07 15:22:55 -0300

    Atividade 2 feita

--- atv/02-funcoes/solucao.c
+++ atv/02-funcoes/solucao.c
@@ -1,1 +1,15 @@
 // TODO implemente aqui uma funcao chamada "solucao"
+
+// edi = int a
+// rsi = long b
+// rdx = long c
+
+int solucao(int a, long b, int *c){
+    a+= a;
+    int retval = b*4 + b;
+    retval += a;
+    *c = retval;
+    retval = b*2 + b;
+    retval += a;
+    return retval;
+}
