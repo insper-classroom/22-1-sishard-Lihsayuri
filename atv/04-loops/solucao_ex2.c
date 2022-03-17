3a8fd1beb32f534aac597f51852ed3f90cebb27d
Lihsayuri <liviasm1@al.insper.edu.br>
2022-03-17 17:14:19 -0300

    Atividade 4 feita

--- atv/04-loops/solucao_ex2.c
+++ atv/04-loops/solucao_ex2.c
@@ -4,3 +4,17 @@
  *
  * Chame sua função de soma_n_solucao */
 
+
+long soma_n_solucao(int a){
+    long c = 0;
+    int retval = 0;
+    
+    while (retval < a){
+        long d = retval;
+        c+=d;
+        retval+=1;
+    }
+
+    return c;
+
+}
--- atv/04-loops/solucao_ex3.c
+++ atv/04-loops/solucao_ex3.c
@@ -2,3 +2,26 @@
  * presente no arquivo ex3.o
  * 
  * Chame sua função de ex3_solucao */
+
+// ecx = variavel local
+// r8d = variavel local
+// rcx = d - parâmetro
+// rdi - a - parametro
+// rdx = fica com resto;
+
+long ex3_solucao(long a, long b, long d){
+    int var1 = 0;
+    long var2 = 0;
+    long retval;
+    while (d < a){
+        retval = d;
+        if ((retval % b) == 0){
+            var2+=d;
+        }
+
+        d +=1;
+    }
+
+    return var2;
+
+}
