754ef8648cfa5d8e7647a850608bdf3f0828b3c2
Lihsayuri <liviasm1@al.insper.edu.br>
2022-03-21 15:08:34 -0300

    Atividade 5 feita!

--- atv/05-variaveis-locais/solucao_ex4.c
+++ atv/05-variaveis-locais/solucao_ex4.c
@@ -1,4 +1,23 @@
 int ex4_solucao(int d) {
     // implemente sua resposta aqui.
     // TODO: conserte valor de retorno e argumentos, se for necessário
+
+    // d é ebx
+    int ebx = d;
+
+    int var1;
+    int var2;
+    scanf("%d %d", &var2, &var1);
+
+    int retval = var2 + 2*var1;
+    return (retval+d);
 }
+
+// rbx é local
+
+// %rdi
+// %rsi
+// %rdx
+// %rcx
+// %r8
+// %r9
--- atv/05-variaveis-locais/solucao_ex5.c
+++ atv/05-variaveis-locais/solucao_ex5.c
@@ -1,4 +1,21 @@
 int ex5_solucao () {
     // implemente sua resposta aqui.
     // TODO: conserte valor de retorno e argumentos, se for necessário
+    int var1 = 1;
+    int ebx = 0;
+
+    while (var1 > 0){
+        int retval = 0;
+        scanf("%d", &var1);
+
+        retval = var1;
+        if (retval > 0){
+            ebx+= retval;
+        }
+    }
+
+    return ebx;
+
 }
+
+
